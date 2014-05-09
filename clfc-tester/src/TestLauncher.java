3031494714eff7465dd8e9ef4ff0faaa4e46b9c8
worgflores <worgflores@gmail.com>
2014-05-09 13:43:34 +0800

    added clfc-tester

---
+++ clfc-tester/src/TestLauncher.java
@@ -0,0 +1,28 @@
+
+import com.rameses.osiris2.test.OsirisTestPlatform;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author wflores
+ */
+public class TestLauncher {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
+        } catch(Exception ex) {;} 
+        
+        Map env = new HashMap(); 
+        env.put("app.host","localhost:8070"); 
+        env.put("app.context","clfc"); 
+        env.put("app.cluster","osiris3"); 
+        env.put("app.debug",true); 
+        
+        env.put("app.custom", "clfc"); 
+        //env.put("ws.host", "localhost:8060");         
+        OsirisTestPlatform.runTest(env, new HashMap(), new HashMap()); 
+    }
+}

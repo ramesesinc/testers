72cc83674e63c72934834efad97f703057978d69
worgflores <worgflores@gmail.com>
2014-09-14 19:58:19 +0800

    updated the etracs-tester

--- etracs-tester/test/testunit/NewEmptyJUnitTest.java
+++ etracs-tester/test/testunit/NewEmptyJUnitTest.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package zzz;
+package testunit;
 
 import com.rameses.service.ScriptServiceContext;
+import groovy.util.AntBuilder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
